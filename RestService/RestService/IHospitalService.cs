35a2df4b1f712c160c419023515edd5cb852a053
Desislav <desso430@gmail.com>
2016-04-08 23:30:33 +0300

    Add Hospital Rest Service

---
+++ RestService/RestService/IHospitalService.cs
@@ -0,0 +1,14 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace RestService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IHospitalService" in both code and config file together.
+    [ServiceContract]
+    public interface IHospitalService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "patient/{id}")]
+        string testGET(string id);
+    }
+}
